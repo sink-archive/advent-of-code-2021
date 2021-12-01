b66463bc037f17bc5b57a78e060100d32faf681f
Cain Atkinson <yellowsink@protonmail.com>
2021-12-01 23:12:48 +0000

    ahahhhhhaahahhhhah im going mad with power

---
+++ 1/2.fsx
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S dotnet fsi --exec
+
+// get an int list
+let raw = fsi.CommandLineArgs.[1].Split()
+          |> Array.map int
+          |> Array.toList
+
+// triples
+raw
+|> List.skip 2
+|> List.mapi (fun i e -> raw.[i] + raw.[i + 1] + e)
+
+// count increases
+|> List.fold
+    (fun (prev, incs) next ->
+        match prev with
+        | None -> (Some(next), incs)
+        | Some (p) ->
+            if next > p then
+                (Some(next), incs + 1)
+            else
+                (Some(next), incs))
+    (None, 0)
+
+// discard the state prev value
+|> snd
+|> printfn "%i"
